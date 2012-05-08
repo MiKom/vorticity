cf59b071c4568672d278495eac67126af4325efe
Miłosz Kosobucki <milosz@kosobucki.pl>
2012-05-08 09:15:21 +0200

    Finished AVR cube application. However it produces bad normals.

--- avr/examples/cube/cube.cpp
+++ avr/examples/cube/cube.cpp
@@ -16,6 +16,23 @@ void usage(char* exName)
 	             	"\t e.g.:" << exName << " 1.0 2.0 0.5 cube.avr\n";
 }
 
+/*
+  Vertex numbering:
+     7             6
+     +-----------  +
+    /|            /|
+  3/ |          2/ |
+  +-------------+  |
+  |  |          |  |
+  |  |          |  |
+  |  +----------|--+
+  | /4          | /5
+  |/            |/
+  +-------------+
+  0             1
+  
+*/
+
 int main(int argc, char** argv)
 {
 	float xsize, ysize, zsize;
@@ -37,20 +54,61 @@ int main(int argc, char** argv)
 	filename = std::string(argv[argvPos++]);
 	
 	AVRFile file;
-	vec3 vertices[8] = {
-		{ xsize, -ysize, -zsize},
-		{ xsize,  ysize, -zsize},
-		{-xsize,  ysize, -zsize},
-		{-xsize, -ysize,  zsize},
-		{ xsize, -ysize,  zsize},
-		{ xsize,  ysize,  zsize},
-		{-xsize,  ysize,  zsize}
+	std::vector<vec3> vertices = {
+		vec3{-xsize, -ysize,  zsize},
+		vec3{ xsize, -ysize,  zsize},
+		vec3{ xsize,  ysize,  zsize},
+		vec3{-xsize,  ysize,  zsize},
+		vec3{-xsize, -ysize, -zsize},
+		vec3{ xsize, -ysize, -zsize},
+		vec3{ xsize,  ysize, -zsize},
+	        vec3{-xsize,  ysize, -zsize}
 	};
-	std::vector<vec3> verticesVec = {
-		vertices[0],vertices[1],vertices[2],vertices[3],
-		vertices[4],vertices[5],vertices[6],vertices[7],
+	
+	//TODO: Fix normals
+	vec3 normals[6] = {
 	};
-	//TODO: implement rest
+	std::vector<vec3> normalsVec = {
+		vec3{0, 0, -1}, vec3{1, 0, 0}, vec3{0, 1,  0},
+		vec3{-1, 0,  0}, vec3{ 0, -1, 0}, vec3{0,  0, 1}
+	};
+	
+	std::vector<AVRFace> faces = {
+		AVRFace{{0, 1, 3}, {0, 0, 0}, {0, 0, 0}},
+		AVRFace{{1, 2, 3}, {0, 0, 0}, {0, 0, 0}},
+		AVRFace{{1, 6, 2}, {3, 3, 3}, {0, 0, 0}},
+		AVRFace{{1, 5, 6}, {3, 3, 3}, {0, 0, 0}},
+		AVRFace{{1, 0, 5}, {2, 2, 2}, {0, 0, 0}},
+		AVRFace{{0, 4, 5}, {2, 2, 2}, {0, 0, 0}},
+		AVRFace{{0, 7, 4}, {1, 1, 1}, {0, 0, 0}},
+		AVRFace{{0, 3, 7}, {1, 1, 1}, {0, 0, 0}},
+		AVRFace{{3, 2, 6}, {4, 4, 4}, {0, 0, 0}},
+		AVRFace{{3, 6, 7}, {4, 4, 4}, {0, 0, 0}},
+		AVRFace{{4, 7, 5}, {5, 5, 5}, {0, 0, 0}},
+		AVRFace{{5, 7, 6}, {5, 5, 5}, {0, 0, 0}} 
+	};
+	
+	AVRMesh mesh;
+	mesh.setName("box");
+	file.addMesh(&mesh);
+	mesh.addVertices(vertices);
+	mesh.addNormals(normalsVec);
+	mesh.addTextCoord(vec2{0.0f, 0.0f});
+	mesh.addFaces(faces);
+	mesh.setMaterialId(0);
+	
+	
+	AVRMaterial material;
+	material.setName("red");
+	material.setDiffuse(0.5f, 0.0f, 0.0f);
+	material.setSpecular(0.9f, 0.9f, 0.9f);
+	material.setAmbient(0.0f, 0.0f, 0.0f);
+	material.setEmissive(1.0f, 0.0f, 0.0f);
+	material.setOpacity(1.0f);
+	material.setSpecularLevel(0.25f);
+	material.setGlossiness(0.15f);
+	
+	file.addMaterial(material);
 	file.save(filename);
 	return 0;
 }
